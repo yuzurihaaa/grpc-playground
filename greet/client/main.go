a2ca1970d4389c0d0541ba884591c8bd384bcd44
yusuf <yusuf@setel.com>
2022-08-23 19:12:57 +0800

    wip: greet stream client -> server

--- greet/client/main.go
+++ greet/client/main.go
@@ -7,6 +7,7 @@ import (
 	pb "grpc-playground/greet/proto"
 	"io"
 	"log"
+	"time"
 )
 
 const addr = "localhost:50051"
@@ -20,10 +21,10 @@ func main() {
 
 	defer conn.Close()
 
-	// Context parsing is overkill, but I'm just playing around.
 	greetClient := pb.NewGreetServiceClient(conn)
 	doGreet(greetClient)
 	listenGreet(greetClient)
+	sendGreets(greetClient)
 }
 
 func doGreet(client pb.GreetServiceClient) {
@@ -64,4 +65,39 @@ func listenGreet(client pb.GreetServiceClient) {
 		log.Printf("listenGreet response %v\n", message.Result)
 	}
 
+}
+
+func sendGreets(client pb.GreetServiceClient) {
+	log.Println("modules.greet - DoGreet")
+
+	reqs := []*pb.GreetRequest{
+		{FirstName: "Kamal"},
+		{FirstName: "Firdaus"},
+		{FirstName: "Hafiz"},
+	}
+
+	stream, err := client.LongGreet(context.Background())
+
+	if err != nil {
+		log.Fatalf("Fail to greet server %v\n", err)
+	}
+
+	for _, req := range reqs {
+
+		log.Printf("sending %v", req)
+
+		err := stream.Send(req)
+		if err != nil {
+			log.Fatalf("Fail to send data %v\n", err)
+		}
+
+		time.Sleep(time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Fail to CloseAndRecv %v\n", err)
+	}
+
+	log.Printf("Greet %s", res.Result)
 }
--- greet/server/modules/greet/greetService.go
+++ greet/server/modules/greet/greetService.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	pb "grpc-playground/greet/proto"
+	"io"
 	"log"
 	"time"
 )
@@ -25,6 +26,32 @@ func (s *Service) Greets(in *pb.GreetRequest, stream pb.GreetService_GreetsServe
 		stream.Send(&pb.GreetResponse{
 			Result: res,
 		})
+	}
+	return nil
+}
+
+func (s *Service) LongGreet(stream pb.GreetService_LongGreetServer) error {
+	log.Print("LongGreet invoked")
+
+	res := ""
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			err := stream.SendAndClose(&pb.GreetResponse{
+				Result: res,
+			})
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Fail to receive data %v\n", err)
+		}
+
+		res += fmt.Sprintf("Hello %s!\n", req.FirstName)
 	}
 	return nil
 }
